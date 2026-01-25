e24e13b8a033f2f291e1461d936520ad3d5fdec2
Anar765 <anar.aliyev181@gmail.com>
2026-01-26 00:31:49 +0400

    feat: Create User Model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            minLength: 1,
+            maxLength: 16
+        },
+        password: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            minLength: 6,
+            maxLength: 30
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const User = mongoose.model("Users", userSchema);
