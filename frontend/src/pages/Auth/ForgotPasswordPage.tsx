5408d24ddfbaabe953f1ebb02345c2e5e80b882a
Anar765 <anar.aliyev181@gmail.com>
2026-02-14 22:20:15 +0400

    feat: Make Forgot and Reset Password Pages responsible for mobile

--- frontend/src/pages/Auth/ForgotPasswordPage.tsx
+++ frontend/src/pages/Auth/ForgotPasswordPage.tsx
@@ -9,7 +9,7 @@ const ForgotPasswordPage = () => {
   return (
     <div className="min-h-screen bg-linear-to-br from-indigo-50 via-white to-purple-50 flex items-center justify-center p-4">
       <div className="w-full max-w-md">
-        <div className="bg-white rounded-2xl shadow-xl border border-gray-200 p-8">
+        <div className="bg-white rounded-2xl shadow-xl border border-gray-200 px-4 py-8 md:p-8">
           <Logo isMobileLogo={false} />
 
           {!emailSent ? <ResetLink setEmailSent={setEmailSent} /> : <SuccessState />}
--- frontend/src/pages/Auth/ResetPasswordPage.tsx
+++ frontend/src/pages/Auth/ResetPasswordPage.tsx
@@ -11,7 +11,7 @@ const ResetPasswordPage = () => {
   return (
     <div className="min-h-screen bg-linear-to-br from-indigo-50 via-white to-purple-50 flex items-center justify-center p-4">
       <div className="w-full max-w-md">
-        <div className="bg-white rounded-2xl shadow-xl border border-gray-200 p-8">
+        <div className="bg-white rounded-2xl shadow-xl border border-gray-200 px-4 py-8 md:p-8">
           <Logo isMobileLogo={false} />
 
           {/* Header */}
