856248ca32ad7afe0767456106e0679fbf386db4
Anar765 <anar.aliyev181@gmail.com>
2026-02-15 15:59:35 +0400

    feat(auth): Add User Login API

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -40,6 +40,47 @@ const registerUser = async (req, res) => {
     }
 }
 
+const loginUser = async(req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({
+                message: "All fields are important"
+            })
+        }
+
+        const existing = await User.findOne({ email });
+
+        if(!existing) {
+            return res.status(404).json({
+                message: "User is not registered"
+            });
+        }
+
+        if(existing.password !== password) {
+            return res.status(403).json({
+                message: "Password is incorrect"
+            });
+        }
+
+        res.status(202).json({
+            message: "Welcome back",
+            user: {
+                username: existing.username,
+                email
+            }
+        })
+
+    } catch(error) {
+        res.status(500).json({
+            error: error.message,
+            message: "Internal Server Error"
+        })
+    }
+}
+
 export {
-    registerUser
+    registerUser,
+    loginUser
 }
