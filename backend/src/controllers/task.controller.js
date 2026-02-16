fefaedc62cba08e6c49fc1e86c5eb1ad0cf4e5fe
Anar765 <anar.aliyev181@gmail.com>
2026-02-16 20:51:47 +0400

    feat(task): Implement delete task API for user

--- backend/src/controllers/task.controller.js
+++ backend/src/controllers/task.controller.js
@@ -108,8 +108,45 @@ const updateTask = async(req, res) => {
     }
 }
 
+const deleteTask = async(req, res) => {
+    try {
+        const { userId, taskId } = req.params;
+
+        if(!mongoose.Types.ObjectId.isValid(userId)) {
+            return res.status(400).json({
+                message: "Invalid user ID format"
+            });
+        }
+
+        if(!mongoose.Types.ObjectId.isValid(taskId)) {
+            return res.status(400).json({
+                message: "Invalid task ID"
+            });
+        }
+
+        const taskObj = await Task.findOneAndDelete({ _id: taskId, userId });
+
+        if(!taskObj) {
+            return res.status(404).json({
+                message: "Task not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Task deleted successfully",
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            error: error.message,
+            message: "Internal Server Error"
+        });
+    }
+}
+
 export {
     createTask,
     getTasks,
-    updateTask
+    updateTask,
+    deleteTask
 }
--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import { createTask, getTasks, updateTask } from "../controllers/task.controller.js";
+import { createTask, deleteTask, getTasks, updateTask } from "../controllers/task.controller.js";
 
 const taskRouter = Router({ mergeParams: true });
 
 taskRouter.route('/create').post(createTask);
 taskRouter.route('/get').get(getTasks);
-taskRouter.route('/update/:taskId').patch(updateTask)
+taskRouter.route('/update/:taskId').patch(updateTask);
+taskRouter.route('/delete/:taskId').delete(deleteTask)
 
 export default taskRouter;
