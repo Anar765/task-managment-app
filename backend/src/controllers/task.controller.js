94e1e0626900862389988ec42ce05bbd9084ebc4
Anar765 <anar.aliyev181@gmail.com>
2026-02-16 20:36:51 +0400

    feat(task): Implement update task API for user

--- backend/src/controllers/task.controller.js
+++ backend/src/controllers/task.controller.js
@@ -47,7 +47,7 @@ const createTask = async(req, res) => {
 const getTasks = async(req, res) => {
     try {
         const { userId } = req.params;
-        
+
         if (!mongoose.Types.ObjectId.isValid(userId)) {
             return res.status(400).json({
                 message: "Invalid user ID"
@@ -64,7 +64,52 @@ const getTasks = async(req, res) => {
     }
 }
 
+const updateTask = async(req, res) => {
+    try {
+
+        const { userId, taskId } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(taskId)) {
+            return res.status(400).json({
+                message: "Invalid task ID"
+            });
+        }
+
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+            return res.status(400).json({
+                message: "Invalid user ID format"
+            });
+        }
+
+        if(Object.keys(req.body).length === 0) {
+            return res.status(400).json({
+                message: "No data provided for update"
+            });
+        }
+
+        // Meaning of { new: true } -> Return the updated document instead of the old one.
+        const taskObj = await Task.findOneAndUpdate({ _id: taskId, userId }, req.body, { new: true });
+
+        if (!taskObj) {
+            return res.status(404).json({
+                message: "Task not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Task updated successfully",
+            task: taskObj
+        });
+    } catch(error) {
+        res.status(500).json({
+            error: error.message,
+            message: "Internal Server Error"
+        });
+    }
+}
+
 export {
     createTask,
-    getTasks
+    getTasks,
+    updateTask
 }
--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { createTask, getTasks } from "../controllers/task.controller.js";
+import { createTask, getTasks, updateTask } from "../controllers/task.controller.js";
 
 const taskRouter = Router({ mergeParams: true });
 
 taskRouter.route('/create').post(createTask);
 taskRouter.route('/get').get(getTasks);
+taskRouter.route('/update/:taskId').patch(updateTask)
 
 export default taskRouter;
