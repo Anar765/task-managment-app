6ead6717b636328d9d7b65d13bf24b244f15f25a
Anar765 <anar.aliyev181@gmail.com>
2026-02-22 17:29:54 +0400

    feat: Integrate create task endpoint with frontend

--- frontend/src/pages/Dashboard/DashboardPage.tsx
+++ frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -2,18 +2,20 @@ import Header from "../../components/Dashboard/Header";
 import SideBar from "../../components/Dashboard/SideBar";
 import StatsCards from "../../components/Dashboard/StatsCards";
 import TaskCards from "../../components/Dashboard/TaskCards";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import type { Task } from "../../types/tasks.type";
 import NewTaskForm from "../../components/Dashboard/NewTaskForm";
 import TasksFilter from "../../components/Dashboard/TasksFilter";
 import type { User } from "../../types/user.type";
 import { useNavigate, useParams } from "react-router-dom";
+import { AppContext } from "../../App";
 
 const DashboardPage = ({ user } : { user: User | undefined }) => {
 
   const [isNewTaskFormOpen, setIsNewTaskFormOpen] = useState(false);
   const { username } = useParams();
   const navigate = useNavigate();
+  const { setTasks } = useContext(AppContext);
 
   useEffect(() => {
     if(user && username !== user.username) {
@@ -21,21 +23,36 @@ const DashboardPage = ({ user } : { user: User | undefined }) => {
     }
   }, [user, username]);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 
-    const formData = new FormData(e.currentTarget)
+    try {
+      const formData = new FormData(e.currentTarget)
 
-    const task: Task = {
-      title: formData.get("title") as string,
-      description: formData.get("description") as string,
-      status: "Not completed",
-      priority: formData.get("priority") as string,
-      category: formData.get("category") as string,
-      date: new Date(formData.get("date") as string)
+      const task: Task = {
+        title: formData.get("title") as string,
+        description: formData.get("description") as string,
+        status: "Not completed",
+        priority: formData.get("priority") as string,
+        category: formData.get("category") as string,
+        date: new Date(formData.get("date") as string)
+      };
+
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/${user?.id}/tasks/create`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(task)
+      });
+
+      const json = await response.json();
+
+      setTasks((prevState) => [...prevState, task]);
+
+      console.log(json);
+    } catch(error) {
+      console.log(error);
     }
-
-    console.log(task);
   }
 
   return (
