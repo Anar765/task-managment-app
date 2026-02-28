d699857a2765a38cf9cfe4aa87e22a9d80803286
Anar765 <anar.aliyev181@gmail.com>
2026-02-28 20:46:55 +0400

    feat: Implement drag-and-drop task reordering with framer-motion

--- frontend/src/components/Dashboard/TaskCards.tsx
+++ frontend/src/components/Dashboard/TaskCards.tsx
@@ -2,10 +2,11 @@ import TaskCard from "./TaskCard";
 import { useContext } from "react";
 import { AppContext } from "../../App";
 import type { FilterParams } from "../../types/filterParams.type";
+import { Reorder } from "framer-motion";
 
 const TaskCards = ({ searchTask, filterParams } : { searchTask: string, filterParams: FilterParams }) => {
 
-    const { tasks } = useContext(AppContext);
+    const { tasks, setTasks } = useContext(AppContext);
     const filteredTasks = tasks.filter((task) => {
         // Check if the search term matches the title
         const matchesSearch = task.title.toLowerCase().includes(searchTask.toLowerCase());
@@ -19,13 +20,18 @@ const TaskCards = ({ searchTask, filterParams } : { searchTask: string, filterPa
     });
 
     return (
-        <>  
-            {tasks.length !== 0 ? (filteredTasks.length !== 0 ? filteredTasks.map((task) => (
-                    <TaskCard
-                        key={task.id}
-                        {...task}
-                    />
-                )) : 
+        <>
+            {tasks.length !== 0 ? (filteredTasks.length !== 0 ? 
+                <Reorder.Group values={tasks} onReorder={setTasks} className="flex flex-col gap-4">
+                    {filteredTasks.map((task) => (
+                    <Reorder.Item value={task} key={task.id}>
+                        <TaskCard
+                            key={task.id}
+                            {...task}
+                        />
+                    </Reorder.Item>
+                    ))}
+                </Reorder.Group> : 
                 <p className="text-center">You don't have tasks with these properties.</p>
             ) : 
                 <p className="text-center">You haven't created any tasks yet.</p>
