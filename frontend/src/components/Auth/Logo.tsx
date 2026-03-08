1f6db5c61b37c1b34aa4ea13e5521d952b58dbad
Anar765 <anar.aliyev181@gmail.com>
2026-03-08 22:40:01 +0400

    feat: Implement Dark Mode for Logo

--- frontend/src/components/Auth/Logo.tsx
+++ frontend/src/components/Auth/Logo.tsx
@@ -5,7 +5,7 @@ const Logo = ({ isMobileLogo }: { isMobileLogo: boolean}) => {
     <>
         {/* Logo */}
         <div className={`${isMobileLogo ? "lg:hidden" : ""} flex items-center gap-3 mb-6`}>
-          <div className="w-10 h-10 bg-indigo-600 rounded-lg flex items-center justify-center">
+          <div className="w-10 h-10 bg-indigo-600 dark:bg-indigo-500 rounded-lg flex items-center justify-center">
               <CheckCircle2 className="w-6 h-6 text-white" />
           </div>
           <h1 className="text-2xl font-bold text-gray-900">DevTasks</h1>
