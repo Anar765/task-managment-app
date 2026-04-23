126eb3fdf8d2d701c4ce1240e0ae88acdc6a6e1c
Anar765 <anar.aliyev181@gmail.com>
2026-04-23 22:37:16 +0400

    feat: Add cookieParser middleware to the Express app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import taskRouter from "./routes/task.route.js";
 
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser());
 app.use(cors({
     origin: `http://localhost:${process.env.FRONTEND_PORT}`,
     credentials: true
