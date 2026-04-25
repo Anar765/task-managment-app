e9f50e2635a3040e8f103cbde9b2cf8525009f8a
Anar765 <anar.aliyev181@gmail.com>
2026-04-25 19:43:36 +0400

    security: Add helmet middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoSanitize from "express-mongo-sanitize";
+import helmet from "helmet";
 import userRouter from "./routes/user.route.js";
 import taskRouter from "./routes/task.route.js";
 
@@ -9,11 +10,12 @@ const app = express();
 
 app.use(express.json())
 app.use(mongoSanitize());
+app.use(helmet());
 app.use(cookieParser());
 app.use(cors({
     origin: `http://localhost:${process.env.FRONTEND_PORT}`,
     credentials: true
-}))
+}));
 
 app.use("/api/users", userRouter);
 app.use("/api/tasks", taskRouter);
