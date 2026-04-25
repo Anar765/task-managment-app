b523f7af6e5067f2994b7a60a4660b87799f9282
Anar765 <anar.aliyev181@gmail.com>
2026-04-25 19:19:49 +0400

    security: Add express-mongo-sanitize middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import mongoSanitize from "express-mongo-sanitize";
 import userRouter from "./routes/user.route.js";
 import taskRouter from "./routes/task.route.js";
 
 const app = express();
 
 app.use(express.json())
+app.use(mongoSanitize());
 app.use(cookieParser());
 app.use(cors({
     origin: `http://localhost:${process.env.FRONTEND_PORT}`,
