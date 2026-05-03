df4e2ea1a8a8c6e01e05876bacf329409802fe7f
Anar765 <anar.aliyev181@gmail.com>
2026-05-03 14:44:40 +0400

    fix: Delete express-mongo-sanitize package to fix an error

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,13 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-// import mongoSanitize from "express-mongo-sanitize";
 import helmet from "helmet";
 import authRouter from "./routes/auth.route.js";
 import taskRouter from "./routes/task.route.js";
 
 const app = express();
 
-app.use(express.json())
-// app.use(mongoSanitize());
+app.use(express.json());
 app.use(helmet());
 app.use(cookieParser());
 app.use(cors({
